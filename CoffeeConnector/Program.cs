c3c039d983b890341d269e16dc949b6e4447ecef
Gereon Godde <981843+Gereon93@users.noreply.github.com>
2023-07-18 13:59:08 +0200

    migrate

---
+++ CoffeeConnector/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeConnector
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
