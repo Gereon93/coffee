9a59a50337556cbfd6ba237213290745394263dd
Gereon <godde.g@me.com>
2023-08-28 19:41:36 +0200

    updates

--- CoffeeWeb/Services/NivonaStatisticsService.cs
+++ CoffeeWeb/Services/NivonaStatisticsService.cs
@@ -2,5 +2,21 @@
 {
     public class NivonaStatisticsService
     {
+
+        private readonly Client _client;
+        public NivonaStatisticsService()
+        {
+            _client = new Client("http://192.168.2.143:8889/", new HttpClient());
+        }
+
+        public async Task<ICollection<NivonaStatisticsModel>> GetStatisticsAsync()
+        {
+            return await _client.GetStatistics2Async();
+        }
+        public async Task AddCoffeePackageAsync(NivonaStatisticsModel newStatistics)
+        {
+            await _client.AddStatisticsAsync(newStatistics);
+        }
+
     }
 }
