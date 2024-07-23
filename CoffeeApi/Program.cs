b5e62e947778b1bbbba26b824ae2fd65ab36e4d3
Gereon Godde <981843+Gereon93@users.noreply.github.com>
2024-07-23 23:07:13 +0200

    added nuget packages

--- CoffeeApi/Program.cs
+++ CoffeeApi/Program.cs
@@ -1,35 +1,43 @@
-using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-string mongoConnectionString = "mongodb://root:example@192.168.2.143:27017/";
-
-// Register MongoClient with DI
-builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace CoffeeApi
 {
-    app.UseDeveloperExceptionPage();
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            string mongoConnectionString = "mongodb://root:example@192.168.2.143:27017/";
+
+            // Register MongoClient with DI
+            builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coffee API V1");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
-});
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
