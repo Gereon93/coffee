d42de3c658fdb23f7d2ed6b9a2834077d53c6439
Gereon Godde <981843+Gereon93@users.noreply.github.com>
2024-08-11 16:17:13 +0200

    finished api

--- CoffeeApi/Controllers/NivonaStatsController.cs
+++ CoffeeApi/Controllers/NivonaStatsController.cs
@@ -1,8 +1,10 @@
 ﻿using CoffeeApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using Newtonsoft.Json;
 using OpenAI;
 using OpenAI.Chat;
+using Tesseract;
 
 
 namespace CoffeeApi.Controllers
@@ -29,6 +31,7 @@ namespace CoffeeApi.Controllers
         {
             if (ModelState.IsValid)
             {
+                await CheckNullFieldsAndUseLastStats(stats);
                 await _nivonaStatsCollection.InsertOneAsync(stats);
 
                 return CreatedAtAction(nameof(GetStatisticsById), new { id = stats.Id }, stats);
@@ -37,6 +40,52 @@ namespace CoffeeApi.Controllers
             {
                 return BadRequest(ModelState);
             }
+        }
+
+        private async Task CheckNullFieldsAndUseLastStats(NivonaStatisticsModel stats)
+        {
+            var newestStatsInDb = await GetNewestStats();
+            if (newestStatsInDb == null)
+            {
+                return;
+            }
+            if (stats.CappuccinoCount == 0)
+            {
+                stats.CappuccinoCount = newestStatsInDb.CappuccinoCount;
+            }
+            if (stats.CoffeeAmericanoCount == 0)
+            {
+                stats.CoffeeAmericanoCount = newestStatsInDb.CoffeeAmericanoCount;
+            }
+            if (stats.CoffeeCount == 0)
+            {
+                stats.CoffeeCount = newestStatsInDb.CoffeeCount;
+            }
+            if (stats.EspressoCount == 0)
+            {
+                stats.EspressoCount = newestStatsInDb.EspressoCount;
+            }
+            if (stats.HotWaterCount == 0)
+            {
+                stats.HotWaterCount = newestStatsInDb.HotWaterCount;
+            }
+            if (stats.LatteMacchiatoCount == 0)
+            {
+                stats.LatteMacchiatoCount = newestStatsInDb.LatteMacchiatoCount;
+            }
+            if (stats.LungoCount == 0)
+            {
+                stats.LungoCount = newestStatsInDb.LungoCount;
+            }
+            if (stats.MilkCount == 0)
+            {
+                stats.MilkCount = newestStatsInDb.MilkCount;
+            }
+            if (stats.MyCoffeeCount == 0)
+            {
+                stats.MyCoffeeCount = newestStatsInDb.MyCoffeeCount;
+            }
+
         }
 
         [HttpGet("GetStatisticsById/{id}")]
@@ -51,6 +100,26 @@ namespace CoffeeApi.Controllers
             }
 
             return Ok(foundStats);
+        }
+
+        [HttpGet("GetLastStatistics")]
+        public async Task<ActionResult<NivonaStatisticsModel>> GetLastStatisticsAsync()
+        {
+            NivonaStatisticsModel? foundStats = await GetNewestStats();
+            if (foundStats == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundStats);
+        }
+
+        private async Task<NivonaStatisticsModel?> GetNewestStats()
+        {
+            var historicalData = await _nivonaStatsCollection.Find(Builders<NivonaStatisticsModel>.Filter.Empty).ToListAsync();
+            var orderdHistoricalData = historicalData.OrderBy(x => x.Timestamp).ToList();
+            var foundStats = orderdHistoricalData.LastOrDefault();
+            return foundStats;
         }
 
         [HttpGet("GetStatistics")]
@@ -76,50 +145,80 @@ namespace CoffeeApi.Controllers
 
             ChatCompletion completion = await _client.CompleteChatAsync(prompt);
 
-            return completion.Content.First().ToString();
+            return completion.Content[0].ToString();
         }
 
         [HttpPost("UploadImage")]
         public async Task<IActionResult> UploadImage(IFormFile file)
         {
+
+
             if (file == null || file.Length == 0)
             {
                 return BadRequest("No file uploaded");
             }
-
+            string ocrText = string.Empty;
             using (var memoryStream = new MemoryStream())
             {
                 await file.CopyToAsync(memoryStream);
                 byte[] imageBytes = memoryStream.ToArray();
+                ocrText = ExtractTextFromImage(imageBytes);
+
 
             }
+            ChatMessage text = $"Can you parse for me this data from tesseract {ocrText} to  the corresponde to the json here please only the technicall without comments so i can parse it with dotnet serializer(from the swaggerfile):" + "NivonaStatisticsModel \"{ \\\"Timestamp\\\": \\\"date\\\", \\\"EspressoCount\\\": 0, \\\"CoffeeCount\\\": 0, \\\"LungoCount\\\": 0, \\\"CappuccinoCount\\\": 0, \\\"LatteMacchiatoCount\\\": 0, \\\"CoffeeAmericanoCount\\\": 0, \\\"MilkCount\\\": 0, \\\"HotWaterCount\\\": 0, \\\"MyCoffeeCount\\\": 0 }\"";
+
+            ChatCompletion completion = await _client.CompleteChatAsync(text);
+
+            // Get image description and extract relevant statistics
+            var description = completion.Content[0].ToString();
+
+            if (description != null)
+            {
+                var statistics = ExtractStatisticsFromDescription(description);
+                CheckNullFieldsAndUseLastStats(statistics);
+                // await _nivonaStatsCollection.InsertOneAsync(statistics);
+            }
+            else
+            {
+                Console.WriteLine("No description could be generated.");
+            }
+
+
 
             return Ok();
         }
-        /*
-                public string ExtractTextFromImage(string imagePath)
-                {
-                    var formRecognizerClient = new FormRecognizerClient(new Uri("<YOUR_FORM_RECOGNIZER_ENDPOINT>"), new AzureKeyCredential("<YOUR_FORM_RECOGNIZER_API_KEY>"));
+        private static NivonaStatisticsModel ExtractStatisticsFromDescription(string description)
+        {
+            // Split the description by comma and iterate over each part
+            var parts = description.Split("```json");
+            var jsonString = parts[1].Split("```")[0];
+            jsonString = jsonString.Replace("\n", string.Empty)
+                          .Replace("\r", string.Empty)
+                          .Replace("\t", string.Empty)
+                          .Trim();
+            var model = JsonConvert.DeserializeObject<NivonaStatisticsModel>(jsonString);
+            model.Timestamp = DateTime.UtcNow;
+            return model;
+        }
 
-                    using (var stream = new FileStream(imagePath, FileMode.Open))
-                    {
-                        var recognizeOptions = new RecognizeContentOptions() { ContentType = "image/jpeg" };
-                        var recognizeResult = await formRecognizerClient.StartRecognizeContentAsync(stream, recognizeOptions).WaitForCompletionAsync();
-                        string extractedText = "";
 
-                        foreach (var page in recognizeResult.Value)
-                        {
-                            foreach (var line in page.Lines)
-                            {
-                                foreach (var word in line.Words)
-                                {
-                                    extractedText += word.Text + " ";
-                                }
-                            }
-                        }
-                        return extractedText.Trim();
-                    return "Hello World";
-                }*/
+        private static string ExtractTextFromImage(byte[] imageBinary)
+        {
+            // Tesseract OCR verwenden, um Text aus dem Bild zu extrahieren
+            try
+            {
+                using var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+                using var img = Pix.LoadFromMemory(imageBinary);
+                using var page = engine.Process(img);
+                return page.GetText();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error during OCR processing: " + ex.Message);
+                return string.Empty;
+            }
+        }
     }
 
 }
