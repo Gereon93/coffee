1528372b1eb4278fc7096c2c83183dcf7d72655d
Gereon Godde <981843+Gereon93@users.noreply.github.com>
2024-08-11 14:02:56 +0200

    durchstich

--- CoffeeWeb/Services/NivonaStatisticsService.cs
+++ CoffeeWeb/Services/NivonaStatisticsService.cs
@@ -2,19 +2,26 @@
 {
     public class NivonaStatisticsService
     {
-
         private readonly Client _client;
+
         public NivonaStatisticsService()
         {
-            _client = new Client("http://192.168.2.143:8889/", new HttpClient());
+#if DEBUG
+            _client = new Client("http://localhost:5174/", new HttpClient());
+#else
+                _client = new Client("http://192.168.2.143:8889/", new HttpClient());
+#endif
         }
 
         public async Task<List<NivonaStatisticsModel>> GetStatisticsAsync()
         {
-            var nivonaStats =  await _client.GetStatistics2Async();
+            var nivonaStats = await _client.GetStatistics2Async();
             return nivonaStats.ToList();
         }
-
-
+        public async Task<string> GetForecastAsync(DateTime date)
+        {
+            var forefast = await _client.ForecastCoffeeStatisticsAsync(date);
+            return forefast;
+        }
     }
 }
